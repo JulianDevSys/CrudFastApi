53c5f8fc60e8559bcaf405ba403d9abc71545007
JulianDevSys <jucastrohenao@gmail.com>
2025-04-08 17:16:34 -0500

    changes

--- main.py
+++ main.py
@@ -63,5 +63,5 @@ import os
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))  # Usa el puerto asignado por Railway
+    port = int(os.environ.get("PORT", 8080))  # Usa el puerto asignado por Railway
     uvicorn.run("main:app", host="0.0.0.0", port=port)
